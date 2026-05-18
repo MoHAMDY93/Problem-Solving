f7fb1546af49fa0c60318524d282bbd22547d879
MoHAMDY93 <140984647+MoHAMDY93@users.noreply.github.com>
2026-05-18 17:40:26 +0300

    Time: 189 ms (14.37%) | Memory: 111.5 MB (13.67%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int , set<int>> mp;
+        for (int i=0 ; i<n ; i++) mp[arr[i]].insert(i);
+        vector<bool> vis(n , false);
+        unordered_set<int> st;
+        // vector<bool> vis_group(n , false);
+        queue<pair<int , int>> q;
+        q.push({0 , 0});
+        while (!q.empty()) {
+            auto [u , dist] = q.front(); q.pop();
+            if (u == n-1) return dist;
+            if (!st.count(arr[u])) {
+                for (auto v : mp[arr[u]]) {
+                    if (vis[v]) continue;
+                    q.push({v , dist+1});
+                    vis[v] = 1;
+                }
+                st.insert(arr[u]);
+            }
+            if (u + 1 < n && !vis[u+1]) vis[u+1] = 1 , q.push({u+1 , dist+1});
+            if (u - 1 >=0 && !vis[u-1]) vis[u-1] = 1 , q.push({u-1 , dist+1});
+        }
+        return -1;
+    }
+};
