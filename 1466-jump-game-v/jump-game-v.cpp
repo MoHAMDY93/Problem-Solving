65e087c898da114eeeeb9f2eabd92c4b64c0dab2
MoHAMDY93 <140984647+MoHAMDY93@users.noreply.github.com>
2026-05-24 05:08:53 +0300

    Time: 39 ms (17.58%) | Memory: 19.3 MB (34.10%) - LeetSync

--- 1466-jump-game-v/jump-game-v.cpp
+++ 1466-jump-game-v/jump-game-v.cpp
@@ -2,30 +2,25 @@ class Solution {
 public:
     int maxJumps(vector<int>& arr, int d) {
         int n = arr.size();
-        vector<int> best(n , -1);
-        auto dfs  = [&](this auto&& dp , int i) -> int {
-            auto& ret = best[i];
-            if (~ret) return ret;
-            ret = 0;
+        vector<int> ord(n);
+        iota(ord.begin() , ord.end() , 0);
+        sort(ord.begin() , ord.end() , [&](int a , int b) {
+            return arr[a] < arr[b];
+        });
+        vector<int> best(n , 1);
+        for (int i : ord) {
             bool ok_right = true , ok_left = true;
             for (int j=1 ; j<=d ; j++) {
                 if (i + j < n && ok_right) {
-                    if (arr[i+j] < arr[i])
-                        ret = max(ret , dp(i + j) + 1);
-                    else 
-                        ok_right = false;
+                    if (arr[i+j] < arr[i]) best[i] = max(best[i] , best[i+j] + 1);
+                    else ok_right = false;
                 }
                 if (i - j >= 0 && ok_left) {
-                    if (arr[i-j] < arr[i])
-                        ret = max(ret , dp(i - j) + 1);
-                    else 
-                        ok_left=  false;
+                    if (arr[i-j] < arr[i]) best[i] = max(best[i] , best[i-j] + 1);
+                    else ok_left = false;
                 }
             }
-            // cout << i << ' ' << ret << '\n';
-            return ret;
-        };
-        for (int i=0 ; i<n ; i++) dfs(i);
-        return *max_element(best.begin() , best.end()) + 1;
+        }
+        return *max_element(best.begin() , best.end());
     }
 };
