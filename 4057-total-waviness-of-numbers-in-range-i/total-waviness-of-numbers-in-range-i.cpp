1f5d896a4ae4032c87b9a3c7f47c1af947f44117
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-06-04 18:37:46 +0300

    Time: 51 ms (47.39%) | Memory: 10.3 MB (33.96%) - LeetSync

---
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int totalWaviness(int num1, int num2) {
+        auto get = [&](int x) -> int {
+            int cnt = 0;
+            string num = to_string(x);
+            for (int i=1 ; i+1 < num.size() ; i++) {
+                if (num[i-1] < num[i] && num[i] > num[i+1]) cnt++;
+                if (num[i-1] > num[i] && num[i] < num[i+1]) cnt++;
+            }
+            return cnt;
+        };
+        int ans = 0;
+        for (int i = num1 ; i<=num2 ; i++) ans += get(i);
+        return ans;
+    }
+};
