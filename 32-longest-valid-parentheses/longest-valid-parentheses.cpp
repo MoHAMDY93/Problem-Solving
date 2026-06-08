e45b3f072b9c15eea581f633cd0111b9dd2f3b2f
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-06-08 16:59:51 +0300

    Time: 3 ms (40.24%) | Memory: 11.8 MB (50.46%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    int memo[30005];
-    string st;
-    int n;
-    int dp(int idx) {
-        if (idx >= n-1) return 0;
-        auto& ret = memo[idx];
-        if (~ret) return ret;
-        ret = 0;
-        if (st[idx] == '(') {
-            int inside_len = dp(idx+1);
-            int cand_match = idx + 1 + inside_len;
-            if (cand_match < n && st[cand_match] == ')') {
-                ret = 2 + inside_len + dp(cand_match + 1);
-            } 
-        }
-        return ret;
-    }
     int longestValidParentheses(string s) {
-        st = s;
-        n = s.size();
-        memset(memo , -1 , sizeof(memo));
+        int n = s.size();
+        stack<int> st;
+        st.push(-1);
         int ans = 0;
         for (int i=0 ; i<n ; i++) {
-            ans = max(ans , dp(i));
+            if (s[i] == '(') st.push(i);
+            else {
+                st.pop();
+                if (st.empty()) {
+                    st.push(i);
+                }
+                ans = max(ans , i - st.top());
+            }
         }
         return ans;
     }
