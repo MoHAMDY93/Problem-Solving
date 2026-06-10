683b16b30bd1129155d9b3f799e0eddca6f06af6
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-06-10 19:26:35 +0300

    Create 129-SumRootToLeafNumbers

---
+++ 129-SumRootToLeafNumbers/129-SumRootToLeafNumbers.cpp
@@ -0,0 +1,17 @@
+// Last updated: 6/10/2026, 7:26:35 PM
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        int sum = 0;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+        while(q.size()) {
+            auto [n, cur] = q.front(); q.pop();
+            cur = cur * 10 + n -> val;
+            if(n -> left) q.push({n -> left, cur});      
+            if(n -> right) q.push({n -> right, cur});
+            if(!n -> left && !n -> right) sum += cur;   
+        }
+        return sum;
+    }
+};
