d55a3fee96005bd910d224cf05c324096e9d69a8
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-06-10 19:18:36 +0300

    Create 2161-StockPriceFluctuation

---
+++ 2161-StockPriceFluctuation/2161-StockPriceFluctuation.java
@@ -0,0 +1,31 @@
+// Last updated: 6/10/2026, 7:18:36 PM
+class StockPrice {
+    TreeMap<Integer, Integer> record = new TreeMap<>();
+    TreeMap<Integer, Set<Integer>> vals = new TreeMap<>();
+
+    public void update(int timestamp, int price) {
+        if (record.containsKey(timestamp)) {
+            int prevPrice = record.get(timestamp);
+            Set<Integer> book = vals.get(prevPrice);
+            book.remove(timestamp);
+            if (book.isEmpty()) {
+                vals.remove(prevPrice);
+            }
+        }
+        vals.putIfAbsent(price, new HashSet<>());
+        vals.get(price).add(timestamp);
+        record.put(timestamp, price);
+    }
+
+    public int current() {
+        return record.lastEntry().getValue();
+    }
+
+    public int maximum() {
+        return vals.lastKey();
+    }
+
+    public int minimum() {
+        return vals.firstKey();
+    }
+}
