77d3519ee5820f15eff4bfc348ea8e8aa2188d90
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-06-10 19:27:51 +0300

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.cpp
@@ -0,0 +1,12 @@
+// Last updated: 6/10/2026, 7:27:50 PM
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+    size_t found = haystack.find(needle);
+    return found;
+    // if (found != std::string::npos) {
+    //     return found;
+    // } else {
+    //     return -1;
+    }
+};
