267b1192b2a08ca5741f726bf0ee57f3d70d0645
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-06-10 19:15:18 +0300

    Create 2892-CheckIfArrayIsGood

---
+++ 2892-CheckIfArrayIsGood/2892-CheckIfArrayIsGood.java
@@ -0,0 +1,11 @@
+// Last updated: 6/10/2026, 7:15:18 PM
+class Solution {
+    public boolean isGood(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length-1;
+        for (int i=1 ; i<=n ; i++) {
+            if (nums[i-1] != i) return false;
+        }
+        return nums[n] == n ? true : false;
+    }
+}
