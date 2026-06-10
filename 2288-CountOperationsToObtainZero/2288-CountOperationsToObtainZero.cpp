a835aaaf2b578ab5944b233302ee8a7fe072dbe7
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-06-10 19:17:41 +0300

    Create 2288-CountOperationsToObtainZero

---
+++ 2288-CountOperationsToObtainZero/2288-CountOperationsToObtainZero.cpp
@@ -0,0 +1,9 @@
+// Last updated: 6/10/2026, 7:17:41 PM
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        if(num1 == 0 || num2 == 0) return 0;
+        if(num1 >= num2) return num1 / num2 + countOperations(num1 % num2 , num2);
+        else return num2 / num1 + countOperations(num1 , num2 % num1);
+    }
+};
