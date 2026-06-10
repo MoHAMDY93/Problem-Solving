be49863c845a95a3e6984255242988b7c7566df7
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-06-10 19:19:12 +0300

    Create 2001-JumpGameVii

---
+++ 2001-JumpGameVii/2001-JumpGameVii.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/10/2026, 7:19:11 PM
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+        int pre = 0;
+        vector<bool> dp(n , false);
+        dp[0] = 1;
+        for (int i=1 ; i<n ; i++) {
+            if (i >= minJump && dp[i - minJump]) pre++;
+            if (i > maxJump && dp[i - maxJump - 1]) pre--;
+            dp[i] = pre > 0 && s[i] == '0';
+        }
+        return dp[n-1];
+    }
+};
