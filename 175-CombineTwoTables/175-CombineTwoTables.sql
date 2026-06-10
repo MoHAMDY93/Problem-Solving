3ec34ffdf48152a880888411c8c913c52020e951
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-06-10 19:26:05 +0300

    Create 175-CombineTwoTables

---
+++ 175-CombineTwoTables/175-CombineTwoTables.sql
@@ -0,0 +1,5 @@
+-- Last updated: 6/10/2026, 7:26:05 PM
+-- Write your PostgreSQL query statement below
+SELECT p.firstName , p.lastName , a.city , a.state FROM Person p
+LEFT JOIN Address a 
+ON p.personId = a.personId
