64d84e215804c60b65b60f2e307e41df15791ac3
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-06-10 19:22:13 +0300

    Create 1245-UserActivityForThePast30DaysI

---
+++ 1245-UserActivityForThePast30DaysI/1245-UserActivityForThePast30DaysI.sql
@@ -0,0 +1,5 @@
+-- Last updated: 6/10/2026, 7:22:12 PM
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE (activity_date > '2019-06-27' AND activity_date <= '2019-07-27')
+GROUP BY activity_date;
