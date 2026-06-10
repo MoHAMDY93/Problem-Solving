78241025f3f978457da94594ffd5cb30cd34081b
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-06-10 19:18:17 +0300

    Create 2199-TwoFurthestHousesWithDifferentColors

---
+++ 2199-TwoFurthestHousesWithDifferentColors/2199-TwoFurthestHousesWithDifferentColors.cpp
@@ -0,0 +1,21 @@
+// Last updated: 6/10/2026, 7:18:16 PM
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n = colors.size();
+        int ans = 0;
+        for (int i=0 ; i<n ; i++) {
+            if (colors[i] ^ colors[n-1]) {
+                ans = max(ans , n-1 - i);
+                break;
+            }
+        }
+        for (int i=n-1 ; i>=0 ; i--) {
+            if (colors[i] ^ colors[0]) {
+                ans = max(ans , i);
+                break;
+            }
+        }
+        return ans ? ans : -1;
+    }
+};
