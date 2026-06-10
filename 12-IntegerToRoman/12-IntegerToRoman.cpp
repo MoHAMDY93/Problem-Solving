851d0073082ec1d10369665c5a32b5d84b5f670f
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-06-10 19:28:13 +0300

    Create 12-IntegerToRoman

---
+++ 12-IntegerToRoman/12-IntegerToRoman.cpp
@@ -0,0 +1,18 @@
+// Last updated: 6/10/2026, 7:28:13 PM
+class Solution {
+public:
+    string intToRoman(int num) {
+        string roman[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int values[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string ans;
+        for(int i=0;i<13;i++)
+        {
+            while(num>=values[i])
+            {
+                ans+=roman[i];
+                num-=values[i];
+            }
+        }
+        return ans;
+    }
+};
