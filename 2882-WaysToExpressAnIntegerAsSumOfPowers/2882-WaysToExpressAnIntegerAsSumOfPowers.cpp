89ddd5b5ccd0c1c0afb2c63cd37235b8cb05721a
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-06-10 19:15:19 +0300

    Create 2882-WaysToExpressAnIntegerAsSumOfPowers

---
+++ 2882-WaysToExpressAnIntegerAsSumOfPowers/2882-WaysToExpressAnIntegerAsSumOfPowers.cpp
@@ -0,0 +1,19 @@
+// Last updated: 6/10/2026, 7:15:19 PM
+class Solution {
+public:
+    int numberOfWays(int n, int x) {
+        long long mod = 1e9 + 7;
+        vector<long long> dp(n + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            long long val = pow(i, x);
+            if (val > n) {
+                break;
+            }
+            for (int j = n; j >= val; j--) {
+                dp[j] = (dp[j] + dp[j - val]) % mod;
+            }
+        }
+        return dp[n];
+    }
+};
