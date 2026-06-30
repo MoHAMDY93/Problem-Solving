4b3fd5616e0ea03363a3e5e80b72fd6bff6df654
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-06-30 18:57:12 +0300

    Create 1358-NumberofSubstringsContainingAllThreeCharacters

---
+++ 1358-NumberofSubstringsContainingAllThreeCharacters/version/cpp/1358-NumberofSubstringsContainingAllThreeCharacters_20260630_185710.cpp
@@ -0,0 +1,16 @@
+// Last updated: 6/30/2026, 6:57:10 PM
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int lastseen[3]={-1,-1,-1};
+        int cnt=0;
+        for(int i=0;i<s.length();i++){
+            lastseen[s[i]-'a']=i;
+            if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1){
+                cnt=cnt+(1+min(lastseen[0],min(lastseen[1],lastseen[2])));
+            }
+        }
+        return cnt;
+        
+    }
+};
