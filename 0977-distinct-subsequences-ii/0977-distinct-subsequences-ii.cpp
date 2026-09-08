8baa8551106290f0528811f6fa89e263d7fa8c04
Mohamed Hamdy <mohamedhamdy4490@gmail.com>
2026-09-09 00:19:40 +0300

    0977-DISTINCT-SUBSEQUENCES-II Stats: Time: 0 ms (100%), Space: 8.9 MB (51.92%) - LeetHub

--- 0977-distinct-subsequences-ii/0977-distinct-subsequences-ii.cpp
+++ 0977-distinct-subsequences-ii/0977-distinct-subsequences-ii.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     int distinctSubseqII(string s) {
         const int MOD = 1e9 + 7;
-
         long long dp = 1;
-        vector<long long> last(26, 0);
-
-        for (char c : s) {
-            int x = c - 'a';
-
-            long long new_dp = (2 * dp - last[x] + MOD) % MOD;
-
-            last[x] = dp;
+        int last[128] = {};
+        for (auto c : s) {
+            int new_dp = (dp * 2 - last[c] + MOD) % MOD;
+            last[c] = dp;
             dp = new_dp;
         }
         return (dp - 1 + MOD) % MOD;
